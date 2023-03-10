ece7d05aeec72dab8475ed407e9f8d1dd3fa67be
jz-scq <scq1144282310@163.com>
2023-03-10 23:46:55 +0800

    :sparkles: element

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,100 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button @click="dialogFormVisible = true">添加</el-button>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column label="日期" width="180">
+        <template slot-scope="scope">
+          <i class="el-icon-time"></i>
+          <span style="margin-left: 10px">{{ scope.row.date }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="姓名" width="180">
+        <template slot-scope="scope">
+          <el-popover trigger="hover" placement="top">
+            <p>姓名: {{ scope.row.name }}</p>
+            <p>住址: {{ scope.row.address }}</p>
+            <div slot="reference" class="name-wrapper">
+              <el-tag size="medium">{{ scope.row.name }}</el-tag>
+            </div>
+          </el-popover>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
+            >编辑</el-button
+          >
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)"
+            >删除</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+    <Dialog
+      ref="dialog"
+      :dialogFormVisible="dialogFormVisible"
+      @close="closeDialog"
+      @add="addDialog"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Dialog from "./components/Dialog.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Dialog,
+  },
+  data() {
+    return {
+      isEdit: 0,
+      dialogFormVisible: false,
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+      ],
+    };
+  },
+  methods: {
+    closeDialog(value) {
+      this.dialogFormVisible = value;
+    },
+    addDialog(from) {
+      let fromValue = { ...from };
+      let params = {
+        date: new Date().toLocaleDateString(),
+        ...fromValue,
+      };
+      if (this.isEdit) {
+        this.tableData.splice(this.isEdit - 1, 1);
+        this.tableData.push(fromValue);
+      } else {
+        this.tableData.push(params);
+      }
+
+      this.$nextTick(() => {
+        this.isEdit = 0;
+      });
+    },
+    handleDelete(index) {
+      this.tableData.splice(index, 1);
+    },
+    handleEdit(index, value) {
+      console.log(index, value);
+      this.dialogFormVisible = true;
+      this.$refs.dialog.getFrom(index, value);
+      this.isEdit = index + 1;
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Dialog.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="dialog">
+    <el-dialog
+      title="收货地址"
+      :visible="dialogFormVisible"
+      :close-on-click-modal="false"
+      @close="close"
+    >
+      <el-form :model="form" ref="ruleForm">
+        <el-form-item
+          label="活动名称"
+          :label-width="formLabelWidth"
+          prop="name"
+        >
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item
+          label="活动区域"
+          :label-width="formLabelWidth"
+          prop="address"
+        >
+          <el-select v-model="form.address" placeholder="请选择活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="close">取 消</el-button>
+        <el-button type="primary" @click="add">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    dialogFormVisible: Boolean,
+  },
+  data: () => ({
+    form: {
+      name: "",
+      address: "",
+    },
+    formLabelWidth: "120px",
+  }),
+  methods: {
+    close() {
+      this.$emit("close", false);
+      this.$nextTick(() => {
+        this.resetForm();
+      });
+    },
+    add() {
+      this.$emit("add", this.form);
+      this.close();
+    },
+    resetForm() {
+      this.$refs.ruleForm.resetFields();
+    },
+    getFrom(index, value) {
+      console.log(index, value);
+      this.form = { ...value };
+    },
+  },
+};
+</script>
+
+<style scoped></style>
